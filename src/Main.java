c498aeb03593b9d7082a455fe54cd4019fcd7556
Rudra Pratap singh <rc195131@gmail.com>
2023-02-01 01:49:19 +0530

    Commited

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        String url="https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=2a767b514413477a92791b5b314936f9&include=minutely";
+        HttpRequest request = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+        HttpClient httpClient = HttpClient.newBuilder().build();
+        HttpResponse<String> httpResponse=httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(httpResponse.statusCode());
+        System.out.println(httpResponse.body());
     }
 }
